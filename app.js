2bc75d6f6381203f36f1ca3397d0c546b726946a
TravisFahey <88859681+TravisFahey@users.noreply.github.com>
2025-02-27 21:37:11 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+const engine = require('ejs-mate');
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error handler (should only be defined once!)
+app.use(function(err, req, res, next) {
+  // Set locals, only providing error details in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page and pass the title
+  res.status(err.status || 500);
+  res.render("error", { 
+      title: "Error Page", // ✅ Ensure title is passed
+      message: err.message,
+      error: res.locals.error // ✅ Only show full error in development
+  });
+});
+
+module.exports = app;
